e37fb6cb641cce690110fdd4636c6de41d528854
Yoscroll <ianhenry.c808@gmail.com>
2017-03-06 08:43:58 -1000

    added console logs

--- app.js
+++ app.js
@@ -4,23 +4,28 @@
 */
 function accessObjectInArray(arr){
 	//Print the parameter being passed through
-
+	console.log(arr);
 	//How can we tell what type of data this is?
 	//By it's notation, the top level is bracket notation, therefore the parameter is an array
 
 	//What methods can we use for this item?
 	//Array methods
-
+	console.log(arr.length);
 	//Access and print the first object in the parameter 
-
+	console.log(arr[0]);
 	//Access and print the third object in the parameter
-
+	console.log(arr[2]);
 	//Iterate through the array to print all the objects in the parameter
-
-		//Can you concatanete a string to an object?
-
+	for (var i = 0; i < arr.length; i++) {
+		// console.log(arr[i]);
+		//Can you concatanete a string to an object? 	NO
+		/*console.log("Inner Object");
+		console.log(arr[i]);*/
+		for (var key in arr){
+			console.log(arr[key]);
+		}
+	}
 		//Iterate through the key:value pairings and print all the keys and their values
-
 	//Clean up your code by creating variable names and setting their values
 }
 
@@ -47,14 +52,29 @@ console.log("\n*** End ***\n")
 * Function that iterates accesses every value in the nested object
 */
 function accessArrayInObject(obj){
-	//Is the parameter an array or an object?
-
+	//Is the parameter an array or an object? OBJECT
+	console.log(obj);
 	//Print the paramter being passed through the function
+	for (var key in obj){
+		console.log("Key:");
+		console.log(key);
+		console.log("Value:")
+		console.log(obj[key]);
+		console.log("\n");
 
+		if (key === "cars"){
+			console.log("Inside conditional for 'cars':");
+			console.log(obj[key]);
+			console.log("\n");
+			console.log("Values for 'cars':");
+			for (var carValue in obj[key]){
+				console.log(obj[key][carValue]);
+			}
+		}
+	}
 	//Iterate through the object
 
 	//Create a conditional to check if the key is "cars"
-
 	//Iterate through and print the values in the key "cars"
 
 	//Iterate through and print the key value pairs for each object in the array "toppings"
